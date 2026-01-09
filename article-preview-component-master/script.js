9f37365ae53ebbefe83cedc486d251550a404cb2
gely25 <angesegoviachavez@gmial.com>
2026-01-09 17:26:07 -0500

    first commit

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,19 @@
+const btn= document.querySelector('.share-toggle-button')
+const card= document.querySelector('.card')
+const footer= document.querySelector('.author')
+
+btn.addEventListener('click', () =>{
+    card.classList.toggle('share-active')
+
+    if (footer.style.display === 'none' ){
+        footer.style.display = 'block'
+    }else{
+        footer.style.display= 'none'
+    }
+
+    
+
+} )
+
+
+
